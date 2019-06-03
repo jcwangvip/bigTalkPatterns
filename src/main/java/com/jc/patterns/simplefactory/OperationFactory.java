1d7335be2dc8633e1108e30ad8efc22f7aa928f7
jiancheng <wangjcc@litsoft.com.cn>
2019-06-04 00:11:18 +0800

    第一次提交

---
+++ src/main/java/com/jc/patterns/simplefactory/OperationFactory.java
@@ -0,0 +1,10 @@
+package com.jc.patterns.simplefactory;
+
+/**
+ * 操作工厂类
+ *
+ * @author jiancheng
+ * @date 2019-6-3
+ */
+public class OperationFactory {
+}
---
+++ src/main/java/com/jc/patterns/simplefactory/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * 本包主要是简单工厂模式
+ *
+ * 涉及的角色有
+ *      1.工厂类
+ *      2.工厂生产的具体类
+ *      3.操作具体类的主类
+ *
+ * @author jiancheng
+ * @date 2019-6-3
+ */
+package com.jc.patterns.simplefactory;
